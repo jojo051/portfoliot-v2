0a4e98dc2156d8c1a0d7ecdbca5d086fb43cfb19
jojo051 <youpi051@yahoo.fr>
2021-03-24 12:10:32 +0100

    tailwindfinish

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className="">
       <Head>
-        <title>hello world</title>
+        <title>portfoliot</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
