3c4696b8b71a220c02b659c2bc581ab84acfd587
jojo051 <youpi051@yahoo.fr>
2021-03-28 23:03:14 +0200

    addpdf

--- src/components/MyCv.jsx
+++ src/components/MyCv.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import Foot from "./Foot";
 import Header from "./Header";
-import PDFViewer from 'pdf-viewer-reactjs';
+import PDFViewer from 'pdf-viewer-reactjs'
 
-const MyCv =()=>{
-  return(
+const MyCv = () => {
+  return (
     <>
-      <Header/>
-      <PDFViewer
+      <Header />
+        <div className="flex justify-center">
+          <PDFViewer
             document={{
-                url: 'https://raw.githubusercontent.com/jojo051/portfoliot-v2/main/src/image/CocktailParadiseAcceuil.png',
+                url: 'https://raw.githubusercontent.com/jojo051/portfoliot-v2/main/src/image/RODIERJoel.pdf',
             }}
-        />
-      <Foot/>
+          />
+        </div>
+      <Foot />
     </>
-  )
-}
+  );
+};
 export default MyCv;
