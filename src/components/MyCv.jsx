6405435910d4b72e50def3ef3a9758806a4390e7
jojo051 <youpi051@yahoo.fr>
2021-03-28 21:45:28 +0200

    add pdf play

--- src/components/MyCv.jsx
+++ src/components/MyCv.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Foot from "./Foot";
 import Header from "./Header";
+import PDFViewer from 'pdf-viewer-reactjs';
 
 const MyCv =()=>{
   return(
     <>
       <Header/>
+      <PDFViewer
+            document={{
+                url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
+            }}
+        />
       <Foot/>
     </>
   )
