0e16b1619f92dd5fceb1d184d327a493c338e793
jojo051 <youpi051@yahoo.fr>
2021-04-14 10:23:41 +0200

    add description sacre campus and fix texte acceuil

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,15 +14,15 @@ const Main = () => {
         <div className="pb-3 sm:pb-5 sm:">
           <h2 className="text-2xl text-center font-black"> BIENVENUE </h2>
           <p className="text-justify px-5 sm:px-20">
-          Après 12 ans de maitre chien à l'armée de l'air en tant que militaire. Passionné d'informatique depuis longtemps. 
+          Après 12 ans de maître chien à l'armée de l'air en tant que militaire, passionné d'informatique depuis longtemps. 
           J’ai décidé de donner du sens à ma carrière professionnelle en me reconvertissant dans les métiers du numérique.
           Autodidacte et curieux de nouvelles technologies, je me suis formé aux bases de la programmation (HTML / CSS et le JS). 
-          J'ai renforcer mes competence en faisaint un formation de 5 mois à la Wild code scool et un stage de 4 mois dans une start up et ensuite obtenu le <span className="font-black">Titre de Développeur web et web mobile</span>.
+          J'ai renforcer mes compétence en faisant une formation de 5 mois à la Wild code scool et un stage de 4 mois dans une start-up et ensuite obtenu le <span className="font-black">Titre de Développeur web et web mobile</span>.
           Je suis actuelement à la recherche d'un emploi.
           </p>
         </div>
         <div className="pb-3 sm:pb-5 sm:w-2/6 lg:w-1/6">
-          <h2 className="text-2xl text-center font-black">SKILL</h2>
+          <h2 className="text-2xl text-center font-black">My SKILL</h2>
           <ul>
             <li className="text-center">React / Next js</li>
             <li className="text-center">Java script</li>
