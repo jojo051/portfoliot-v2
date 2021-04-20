7b1a568a1894fdb5b18bbda10990f98b525320e0
jojo051 <youpi051@yahoo.fr>
2021-04-20 12:49:53 +0200

    add description tonton sommelier alpha

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoHome from "../image/logo-homev2.jpeg"
 
 const Foot = () => {
   return (
     <div className="">
       <hr></hr>
       <div className="bg-primary text-primary flex items-center justify-between text-center px-3 py-1 sm:p-5 lg:p-5">
-        <Link className="text-2xl" to="/">
-          <p>RODIER Joël</p>
+      <Link className="text-2xl flex" to="/"><img src={logoHome} alt="logo-home"width="32px"/>
+          <h1>RODIER Joël</h1>
         </Link>
         <div className="flex justify-end">
           <a
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../image/logo-homev2.jpeg"
 const Header = () => {
   return (
     <>
       <div className="bg-primary text-primary flex items-center justify-between text-center px-3 py-1 sm:p-5 lg:p-5">
-        <Link className="text-2xl" to="/">
+        <Link className="text-2xl flex" to="/"><img src={logo} width="32px"/>
           <h1>RODIER Joël</h1>
         </Link>
         <div className="flex justify-end">
